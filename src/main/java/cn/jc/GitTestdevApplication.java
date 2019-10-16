593c74f09190f8382410dab8025c13fad98a59b6
user007 <22589675962@qq.com>
2019-10-17 01:31:58 +0800

    Initial commit

---
+++ src/main/java/cn/jc/GitTestdevApplication.java
@@ -0,0 +1,13 @@
+package cn.jc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitTestdevApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitTestdevApplication.class, args);
+    }
+
+}
