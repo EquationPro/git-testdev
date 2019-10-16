470f3c9c4a733c25a177f0dd3e4db809d356ec51
user007 <22589675962@qq.com>
2019-10-17 01:39:27 +0800

    future update

--- src/main/java/cn/jc/cntroller/GitController.java
+++ src/main/java/cn/jc/cntroller/GitController.java
@@ -8,5 +8,6 @@ public class GitController {
     public static void main(String[] args) {
         System.out.println("git test01");
         System.out.println("git dev test01");
+        System.out.println("git future test01");
     }
 }
